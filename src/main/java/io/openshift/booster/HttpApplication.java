f26b3d3cb0efd954980b09c04599bb318c88c453
Shoubhik Bose <sbose78@gmail.com>
2017-09-04 18:39:01 +0000

    Pushed from browser based IDE

--- src/main/java/io/openshift/booster/HttpApplication.java
+++ src/main/java/io/openshift/booster/HttpApplication.java
@@ -11,7 +11,7 @@ import static io.vertx.core.http.HttpHeaders.CONTENT_TYPE;
 
 public class HttpApplication extends AbstractVerticle {
 
-  protected static final String template = "Hey boy :)   %s!";
+  protected static final String template = "Edited from browser IDE :)   %s!";
 
   @Override
   public void start(Future<Void> future) {
